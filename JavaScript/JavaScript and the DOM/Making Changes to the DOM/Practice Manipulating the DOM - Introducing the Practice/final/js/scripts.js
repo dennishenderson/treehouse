b7bd4a773199781863908db124a9b23e673f3e3e
Dennis Henderson <dennismhenderson@gmail.com>
2019-06-08 06:53:22 -0400

    Updated final with additional notes and the prepend function

--- JavaScript/JavaScript and the DOM/Making Changes to the DOM/Practice Manipulating the DOM - Introducing the Practice/final/js/scripts.js
+++ JavaScript/JavaScript and the DOM/Making Changes to the DOM/Practice Manipulating the DOM - Introducing the Practice/final/js/scripts.js
@@ -1,40 +1,50 @@
 // 1: Set the text of the <h1> element
-const title = document.querySelector('h1');
-title.textContent = "My Activities List";
+const h1 = document.querySelector('h1');
+// h1.innerHTML = "I'm a BOSS";
+h1.textContent = "Test";
 
 // 2: Set the color of the <h1> to a different color
-title.style.color = 'dodgerblue';
+h1.style.color = 'orange';
 
 // 3: Set the content of the '.desc' paragraph
 // The content should include at least one HTML tag
-const desc = document.querySelector('.desc');
-desc.innerHTML = "A list of <em>fun</em> things I want to do today";
+const divDesc = document.querySelector('.desc');
+divDesc.innerHTML = 'This is a <b>BOLD</b> paragraph';
 
 // 4: Set the class of the <ul> to 'list'
-const list = document.querySelector('ul');
-list.className = 'list';
+const ul = document.querySelector('ul');
+ul.className = 'list';
 
 // 5: Create a new list item and add it to the <ul>
-const item = document.createElement('li');
-item.innerHTML = "<input> Eat ice cream";
-list.appendChild(item);
+const li = document.createElement('li');
+li.innerHTML = '<input> Waffles';
+ul.appendChild(li);
+
+/* The below code is what I came up with and it works, but there is a far easier
+   way to do this using the above code...
+   The differences is that textContent does not parse HTML tags, innerHTML does.
+
+const inputNew = document.createElement('input');
+li.textContent = " Waffles";
+li.prepend(inputNew);
+ul.appendChild(li);
+*/
 
 // 6: Change all <input> elements from text fields to checkboxes
 const input = document.getElementsByTagName('input');
-for (let i = 0; i < input.length; i += 1) {
-  input[i].type = 'checkbox';
+for (let i = 0; i < input.length; i++) {
+  input[i].type = "checkbox";
 }
 
 // 7: Create a <button> element, and set its text to 'Delete'
 // Add the <button> inside the '.extra' <div>
-const deleteButton = document.createElement('button');
-deleteButton.textContent = 'Delete';
-
-const extra = document.querySelector('.extra');
-extra.appendChild(deleteButton);
+const divExtra = document.querySelector('.extra');
+const buttonDelete = document.createElement('button');
+buttonDelete.textContent = "Delete";
+divExtra.appendChild(buttonDelete);
 
 // 8: Remove the '.extra' <div> element from the DOM when a user clicks the 'Delete' button
-const container = document.querySelector('.container');
-deleteButton.addEventListener('click', () => {
-  container.removeChild(extra);  
+const divContainer = document.querySelector('.container');
+buttonDelete.addEventListener('click', () => {
+  divContainer.removeChild(divExtra);
 });
