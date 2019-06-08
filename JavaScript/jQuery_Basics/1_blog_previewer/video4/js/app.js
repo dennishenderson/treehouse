e56a2c63222158f77830cfdad40fc275a634f59f
Dennis Henderson <dennismhenderson@gmail.com>
2019-06-08 19:21:18 -0400

    Updated Files

---
+++ JavaScript/jQuery_Basics/1_blog_previewer/video4/js/app.js
@@ -0,0 +1,12 @@
+// $('#flashMessage').hide();
+// $('#flashMessage').slideDown(1000);
+// $('#flashMessage').delay(3000);
+// $('#flashMessage').slideUp();
+
+// $('#flashMessage').hide().slideDown(1000).delay(3000).slideUp();
+
+$('#flashMessage')
+  .hide()
+  .slideDown(1000)
+  .delay(3000)
+  .slideUp();
---
+++ JavaScript/jQuery_Basics/challenge/challenge1/app.js
@@ -0,0 +1,2 @@
+// document.querySelector('.profile-header').style.display = 'none';
+$('.profile-header').hide().delay(2000).fadeIn();
