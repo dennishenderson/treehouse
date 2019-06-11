c207c999fe145205f18f6f781d50aa605790a339
Dennis Henderson <dennismhenderson@gmail.com>
2019-06-11 14:10:20 -0400

    updated practice handling events jquery with custom bonus features

--- JavaScript/jQuery_Basics/practice/practice_handling_events_jquery/solutions/js/app.js
+++ JavaScript/jQuery_Basics/practice/practice_handling_events_jquery/solutions/js/app.js
@@ -1,23 +1,29 @@
-// 1. When the user's mouse passes over the text "How are these ranked?", hide/show the text inside the #helpText span element. (Hint: use the jQuery toggle() method inside your event handler) 
-$('#helpText').on('mouseover', function(){
+// 1. Hide/show the text inside the #helpText span element when the user's mouse passes over the text "How are these ranked?" (Hint: use the jQuery toggle() method)
+$('#helpText').on('mouseover', () => {
   $('#helpText span').toggle();
 });
 
-// 2. Append a new restaurant to the restaurant list when the user enters a value into the #newRestaurant input and clicks on the #addNew button. 
-$('#addNew').on('click', function(){
-  const newRestaurant = $('#newRestaurantInput').val();
-  $('ul').append(newRestaurant);
- });
-
-  // BONUS: Before appending the new restaurant to the list, use string concatentation to make sure the new restaurant name is surrounded by an opening and closing <li> tag. 
-  //$('ul').append('<li>' + newRestaurant + '</li>');
+// 2. Append a new restaurant to the restaurant list when the user enters a value into the #newRestaurant input and clicks on the #addNew button.
+$('#addNew').on('click', function() {
+  const newRestaurantInput = $('#newRestaurantInput').val();
+  // BONUS: Before appending the new restaurant to the list, use string concatentation to make sure the new restaurant name is surrounded by an opening and closing <li> tag.
+  const newLi = $('ul').append(`<li>${newRestaurantInput}</li>`);
+  // BONUS: clear the value
+  $('#newRestaurantInput').val('');
+});
+// or if pressing enter after typing a name
+$('#newRestaurantInput').keypress(function(event){
+    if(event.which == 13){//Enter key pressed
+        $('#addNew').click();//Trigger addNew button click event
+    }
+});
 
 // 3. Add the class "strike" to the restaurant name each time it's double clicked. Use event delegation on the ul element to listen for a click event on the ul's list items
-$('ul').on('dblclick', 'li', function(event){
-  $(event.target).addClass('strike');
+$('ul').on('dblclick', 'li', function() {
+  // $(event.target).addClass('strike');
+
+  //BONUS: Refactor challenge 3 using the "this" keyword.
+  //BONUS: Look up a jQuery method that will let you toggle the .strike class on and off.
+  $(this).toggleClass('strike');
+
 });
-//BONUS: Refactor your code above using the "this" keyword.
-//BONUS: Look up a jQuery method that will let you toggle the .strike class on and off. 
-//$('ul').on('dblclick', 'li', function(){
-//  $(this).toggleClass('strike);
-//});
