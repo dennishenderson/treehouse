ee02a723607d1d5384b825d39eeb051927b74bd0
Dennis Henderson <dennismhenderson@gmail.com>
2019-06-17 20:48:22 -0400

    Node.js Basics Intro

---
+++ JavaScript/Node.js Basics/1 - Introduction-to-Node.js/app.js
@@ -0,0 +1,3 @@
+console.log('Hello World!!!');
+console.error('Oops something went wrong');
+console.dir({name: 'Dennis', age: 36});
---
+++ JavaScript/Node.js Basics/1 - Introduction-to-Node.js/challenge1.js
@@ -0,0 +1,6 @@
+const name = "Andrew";
+const details = { favouriteLanguage: "JavaScript", age: 33, children: 3 }
+const errorMessage = "Something bad has occurred";
+console.log(name);
+console.dir(details);
+console.error(errorMessage);
